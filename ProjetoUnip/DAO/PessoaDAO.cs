0efb06195f5912ad8437753c81f5973880b3eae5
hanakathy <khanamartins@gmail.com>
2022-11-12 17:49:00 -0400

    adicionados DAO e context

---
+++ ProjetoUnip/DAO/PessoaDAO.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+using ProjetoUnip.Models;
+using ProjetoUnip.Data;
+using System.Reflection.Metadata;
+
+namespace ProjetoUnip.Interfaces
+{
+    public interface PessoaDAO : IDisposable
+    {
+        
+        //Criar Pessoa
+        public bool inserir(Pessoa p)
+        {
+            return false;
+        }
+
+        //Alterar Pessoa
+        public bool alterar(Pessoa p)
+        {
+            return false;
+        }
+
+        //Pesquisar por CPF
+        public Pessoa consultar(long cpf)
+        {
+            Pessoa p = new Pessoa();
+            return p;
+        }
+
+        //Excluir Pessoa
+        public bool excluir(Pessoa p)
+        {
+            return false;
+        }
+    }
+}
---
+++ ProjetoUnip/Data/ProjetoUnipContext.cs
@@ -0,0 +1,25 @@
+﻿using ProjetoUnip.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoUnip.Data
+{
+    public class ProjetoUnipContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySQL(@"Server=(localhost)\root;Database=projetounip;Trusted_Connection=True");
+            }
+        }
+
+        public ProjetoUnipContext(DbContextOptions<ProjetoUnipContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Telefone> Telefone { get; set; }
+        public DbSet<TipoTelefone> TipoTelefone { get; set; }
+    }
+}
